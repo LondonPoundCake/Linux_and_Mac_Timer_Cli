6c81bdccbc532f470c6e9554fa56de355e44f944
LondonPoundCake <ak-47_trigagun@hotmail.co.uk>
2025-05-15 19:59:32 +0100

    + Working MacOs.

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,8 @@
 #include <termios.h>
 // These will go inside of the application_manager function.
 #include "../include/utility.hpp"
-#include "../include/cli.hpp"
-
 #include "../include/timer.hpp"
-
+#include "../include/cli.hpp"
 // Need to define my own parameters so now i need to create a diagram of a basic flowloop of what my application should be able to do.
 
 
@@ -20,22 +18,17 @@ int main(int argc, char * argv[])
     //1] This is going be turned in to a cli function.
 
     cli commandlineinterface("CLI Timer project");
-
-
+    // At some point the below lines 22 - 27 should be all in one class. 
     std::cout << "1]. Check if we have a 2nd arg" << std::endl;
     if(argc < 2)
     {
         std::cout << "Json File " << argv[0] << " " << std::endl;
         return 1;
     }
-
     // 2] .
     // This is going to be turned in to a utility function.
     // my end goal is to have a manager class that can take bot of these.
     // End goal CLI + Utility = ApllicationManager.
-
-
-
     utility util(argv[1],true);
     timer timer_object;
 
@@ -51,12 +44,8 @@ int main(int argc, char * argv[])
             std::cout << "Key is pressed" << std::endl;
             break;
         }
-
     }
-
     util.set_terminal_mode(false);
     std::cout << "." << std::flush;
-
     std::cout << "Finished" << std::endl;
-
 }
